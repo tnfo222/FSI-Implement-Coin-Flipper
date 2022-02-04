83e7755fecf099e2a1d8992ae5351f9cb8ee8c0a
tnfo222 <github.com/tnfo222/psychic-disco>
2022-02-03 21:01:06 -0500

    Initial Release of the Penny Flip Game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,43 @@
-// TODO: Declare any global variables we need
+let numberOfHeads = 0
+let numberOfTails = 0
+let headsPercentage = 0
+let tailsPercentage = 0
+let totalFlips = 0
 
+//let images = ['./assets/images/penny-heads.jpg', './assets/images/penny-tails.jpg']
+let images = [
+    {
+        imagePath: './assets/images/penny-heads.jpg',
+        side: 'heads',
+        message: 'You flipped Heads!'
+    },
+    {
+        imagePath: './assets/images/penny-tails.jpg',
+        side: 'tails' ,
+        message: 'You flipped Tails!'
+    }     
+    ]
 
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+         document.getElementById('flip').addEventListener('click', function() {
+             let randomImage = images[Math.floor(Math.random() * images.length)]
+             document.getElementById('penny-img').setAttribute('src', randomImage.imagePath)
+                if (randomImage.side === 'heads' ) {
+                    numberOfHeads++
+                    document.querySelector('.message-container').textContent = randomImage.message
+                    document.querySelector('#heads').textContent = numberOfHeads
+                }
+                else {
+                    numberOfTails++
+                    document.querySelector('.message-container').textContent = randomImage.message
+                    document.querySelector('#tails').textContent = numberOfTails
+                }
 
-    // TODO: Add event listener and handler for flip and clear buttons
+                totalFlips++
 
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
-
+                headsPercentage = Math.round((numberOfHeads / totalFlips * 100))
+                tailsPercentage = Math.round((numberOfTails / totalFlips * 100))
+                document.getElementById('heads-percent').textContent = headsPercentage + '%'
+                document.getElementById('tails-percent').textContent = tailsPercentage + '%'
+         }) 
 })
